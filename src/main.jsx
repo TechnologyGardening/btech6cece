d0d897b63864af178bbf358e635e989a5466bfaa
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2024-12-19 11:35:26 +0530

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,5 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>Hello World!!!!!</StrictMode>
+  <StrictMode>Hello World 123456!!!!!</StrictMode>
 );
