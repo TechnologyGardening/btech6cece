474e7d0db5a4f94d886378420fa7805db9e113c3
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2024-12-26 10:53:38 +0530

    lec2612

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client";
-import MyComponent from "./comp1";
+// import MyComponent from "./comp1";
+// import StateExample from "./lec2612/stateexmp";
+import StateExample2 from "./lec2612/statemani";
 
 createRoot(document.getElementById("root")).render(
   <div>
-    <MyComponent />
+    <StateExample2 />
   </div>
 );
