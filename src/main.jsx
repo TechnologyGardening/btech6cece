ac413f7b287f704df168a606fdbe0c215e9d6596
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2024-12-26 11:25:09 +0530

    compstate

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
 // import MyComponent from "./comp1";
 // import StateExample from "./lec2612/stateexmp";
-import StateExample2 from "./lec2612/statemani";
+// import StateExample2 from "./lec2612/statemani";
+import IterableExample from "./lec2612/objiter";
 
 createRoot(document.getElementById("root")).render(
   <div>
-    <StateExample2 />
+    <IterableExample />
   </div>
 );
