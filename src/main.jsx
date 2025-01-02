967c70cba43606784271aa23be2978e8705c2557
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2025-01-02 11:30:55 +0530

    merged class C D E

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import { createRoot } from "react-dom/client";
-// import MyComponent from "./comp1";
-// import StateExample from "./lec2612/stateexmp";
-// import StateExample2 from "./lec2612/statemani";
-import IterableExample from "./lec2612/objiter";
+import StateExmp1 from "./lec0201/stateexmp1";
 
 createRoot(document.getElementById("root")).render(
   <div>
-    <IterableExample />
+    <StateExmp1 />
   </div>
 );
